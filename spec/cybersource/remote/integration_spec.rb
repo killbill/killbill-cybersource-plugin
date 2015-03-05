19dde912f93f81b6e3d18dd60a42a6d457a08f97
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-03-05 11:51:10 -0800

    plugin: update to latest generator template
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- spec/cybersource/remote/integration_spec.rb
+++ spec/cybersource/remote/integration_spec.rb
@@ -23,10 +23,10 @@ describe Killbill::Cybersource::PaymentPlugin do
     @plugin.conf_dir     = File.expand_path(File.dirname(__FILE__) + '../../../../')
     @plugin.start_plugin
 
-    @pm         = create_payment_method(::Killbill::Cybersource::CybersourcePaymentMethod, nil, @call_context.tenant_id)
+    @properties = []
+    @pm         = create_payment_method(::Killbill::Cybersource::CybersourcePaymentMethod, nil, @call_context.tenant_id, @properties)
     @amount     = BigDecimal.new('100')
     @currency   = 'USD'
-    @properties = []
 
     kb_payment_id = SecureRandom.uuid
     1.upto(6) do
@@ -39,13 +39,15 @@ describe Killbill::Cybersource::PaymentPlugin do
   end
 
   it 'should be able to charge a Credit Card directly and calls should be idempotent' do
+    properties = build_pm_properties
+
     # We created the payment method, hence the rows
     Killbill::Cybersource::CybersourceResponse.all.size.should == 1
     Killbill::Cybersource::CybersourceTransaction.all.size.should == 0
 
-    payment_response = @plugin.purchase_payment(@pm.kb_account_id, @kb_payment.id, @kb_payment.transactions[0].id, @pm.kb_payment_method_id, @amount, @currency, @properties, @call_context)
+    payment_response = @plugin.purchase_payment(@pm.kb_account_id, @kb_payment.id, @kb_payment.transactions[0].id, @pm.kb_payment_method_id, @amount, @currency, properties, @call_context)
+    payment_response.status.should eq(:PROCESSED), payment_response.gateway_error
     payment_response.amount.should == @amount
-    payment_response.status.should == :PROCESSED
     payment_response.transaction_type.should == :PURCHASE
 
     responses = Killbill::Cybersource::CybersourceResponse.all
@@ -81,70 +83,70 @@ describe Killbill::Cybersource::PaymentPlugin do
 
   it 'should be able to charge and refund' do
     payment_response = @plugin.purchase_payment(@pm.kb_account_id, @kb_payment.id, @kb_payment.transactions[0].id, @pm.kb_payment_method_id, @amount, @currency, @properties, @call_context)
+    payment_response.status.should eq(:PROCESSED), payment_response.gateway_error
     payment_response.amount.should == @amount
-    payment_response.status.should == :PROCESSED
     payment_response.transaction_type.should == :PURCHASE
 
     # Try a full refund
     refund_response = @plugin.refund_payment(@pm.kb_account_id, @kb_payment.id, @kb_payment.transactions[1].id, @pm.kb_payment_method_id, @amount, @currency, @properties, @call_context)
+    refund_response.status.should eq(:PROCESSED), refund_response.gateway_error
     refund_response.amount.should == @amount
-    refund_response.status.should == :PROCESSED
     refund_response.transaction_type.should == :REFUND
   end
 
   it 'should be able to auth, capture and refund' do
     payment_response = @plugin.authorize_payment(@pm.kb_account_id, @kb_payment.id, @kb_payment.transactions[0].id, @pm.kb_payment_method_id, @amount, @currency, @properties, @call_context)
+    payment_response.status.should eq(:PROCESSED), payment_response.gateway_error
     payment_response.amount.should == @amount
-    payment_response.status.should == :PROCESSED
     payment_response.transaction_type.should == :AUTHORIZE
 
     # Try multiple partial captures
     partial_capture_amount = BigDecimal.new('10')
     1.upto(3) do |i|
       payment_response = @plugin.capture_payment(@pm.kb_account_id, @kb_payment.id, @kb_payment.transactions[i].id, @pm.kb_payment_method_id, partial_capture_amount, @currency, @properties, @call_context)
+      payment_response.status.should eq(:PROCESSED), payment_response.gateway_error
       payment_response.amount.should == partial_capture_amount
-      payment_response.status.should == :PROCESSED
       payment_response.transaction_type.should == :CAPTURE
     end
 
     # Try a partial refund
     refund_response = @plugin.refund_payment(@pm.kb_account_id, @kb_payment.id, @kb_payment.transactions[4].id, @pm.kb_payment_method_id, partial_capture_amount, @currency, @properties, @call_context)
+    refund_response.status.should eq(:PROCESSED), refund_response.gateway_error
     refund_response.amount.should == partial_capture_amount
-    refund_response.status.should == :PROCESSED
     refund_response.transaction_type.should == :REFUND
 
     # Try to capture again
     payment_response = @plugin.capture_payment(@pm.kb_account_id, @kb_payment.id, @kb_payment.transactions[5].id, @pm.kb_payment_method_id, partial_capture_amount, @currency, @properties, @call_context)
+    payment_response.status.should eq(:PROCESSED), payment_response.gateway_error
     payment_response.amount.should == partial_capture_amount
-    payment_response.status.should == :PROCESSED
     payment_response.transaction_type.should == :CAPTURE
   end
 
   it 'should be able to auth and void' do
     payment_response = @plugin.authorize_payment(@pm.kb_account_id, @kb_payment.id, @kb_payment.transactions[0].id, @pm.kb_payment_method_id, @amount, @currency, @properties, @call_context)
+    payment_response.status.should eq(:PROCESSED), payment_response.gateway_error
     payment_response.amount.should == @amount
-    payment_response.status.should == :PROCESSED
     payment_response.transaction_type.should == :AUTHORIZE
 
     payment_response = @plugin.void_payment(@pm.kb_account_id, @kb_payment.id, @kb_payment.transactions[1].id, @pm.kb_payment_method_id, @properties, @call_context)
-    payment_response.status.should == :PROCESSED
+    payment_response.status.should eq(:PROCESSED), payment_response.gateway_error
     payment_response.transaction_type.should == :VOID
   end
 
   it 'should be able to auth, partial capture and void' do
     payment_response = @plugin.authorize_payment(@pm.kb_account_id, @kb_payment.id, @kb_payment.transactions[0].id, @pm.kb_payment_method_id, @amount, @currency, @properties, @call_context)
+    payment_response.status.should eq(:PROCESSED), payment_response.gateway_error
     payment_response.amount.should == @amount
-    payment_response.status.should == :PROCESSED
     payment_response.transaction_type.should == :AUTHORIZE
 
     partial_capture_amount = BigDecimal.new('10')
     payment_response       = @plugin.capture_payment(@pm.kb_account_id, @kb_payment.id, @kb_payment.transactions[1].id, @pm.kb_payment_method_id, partial_capture_amount, @currency, @properties, @call_context)
+    payment_response.status.should eq(:PROCESSED), payment_response.gateway_error
     payment_response.amount.should == partial_capture_amount
-    payment_response.status.should == :PROCESSED
     payment_response.transaction_type.should == :CAPTURE
 
     payment_response = @plugin.void_payment(@pm.kb_account_id, @kb_payment.id, @kb_payment.transactions[2].id, @pm.kb_payment_method_id, @properties, @call_context)
-    payment_response.status.should == :PROCESSED
+    payment_response.status.should eq(:PROCESSED), payment_response.gateway_error
     payment_response.transaction_type.should == :VOID
   end
 end
